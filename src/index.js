d5f890a5a6c341cc0c40c98dc7426ea9307a5848
TemaTerbi <tematerbi@mail.ru>
2021-11-29 18:36:07 +0300

    Setup full webpack

---
+++ src/index.js
@@ -0,0 +1,2 @@
+import './js/sum'
+import './sass/style.sass'
---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+
+
+module.exports = {
+  mode: "development",
+  watch: true,
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename:  '[name].js',
+  },
+  plugins: [new MiniCssExtractPlugin(
+    {
+      filename: 'style.css'
+    }
+    ),
+    new HtmlWebpackPlugin({
+      template: './src/index.pug',
+    }),
+  ],
+  module: {
+    rules: [
+    {
+      test: /\.m?js$/,
+      exclude: /(node_modules)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ["@babel/preset-env"]
+        }
+      }
+    },
+    {
+      test: /\.css$/i,
+      use: [MiniCssExtractPlugin.loader, "css-loader"],
+    },
+    {
+      test: /\.s[ac]ss$/i,
+      use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+    },
+    {
+      test: /\.pug$/,
+      loader: 'pug-loader',
+    },
+    {
+      test: /\.(woff|woff2|eot|ttf|otf)$/i,
+      type: 'asset/resource'
+    },
+    {
+      test: /\.(png|svg|jpg|jpeg|gif)$/i,
+      type: 'asset/resource'
+    },
+  ],
+  },
+}
